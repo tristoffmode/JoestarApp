9dc34c337f80be031b800e257568d5aaaae6596a
Bleou kessin <kessinbleou@gmail.com>
2026-03-18 12:19:44 +0100

    changement des noms de fichiers activity

--- app/src/main/java/com/example/joestarapp/LabyrintheActivity.java
+++ app/src/main/java/com/example/joestarapp/LabyrintheActivity.java
@@ -13,12 +13,7 @@ public class LabyrintheActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_labyrinthe);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
     }
 }
